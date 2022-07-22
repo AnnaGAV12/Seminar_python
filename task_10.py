25bae0596180ee4ccf324aed4a820525894bf6fb
Anna <ds_anna@bk.ru>
2022-07-22 16:27:25 +0300

    присутствует ли в заданном списке строк некое число.

--- task_10.py
+++ task_10.py
@@ -6,7 +6,7 @@ str_list = ['12asd36', '256', 'dsds89358', '5698a']
 s_nym = input('Enter the number: ')
 is_Found = False
 
-for item in str_list:
+for item in str_list:# in дает поиск именно в строке
     print(item)
     if item.__contains__(s_nym):
         is_Found = True
