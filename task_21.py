26c8044c94e98698ed7222c460ae69e1e646b81e
Anna <ds_anna@bk.ru>
2022-08-01 19:46:59 +0300

    matrix XO

---
+++ task_21.py
@@ -0,0 +1,8 @@
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix]))
+# for row in matrix:
+#     for item in row:
+#         ''.join('{:4}'.format(item))
+#     print('\n')
+
+
